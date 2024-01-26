69a6bd00b0009ed431e65698a62d47864a60b9cc
mjunaid249 <junaidch249249@gmail.com>
2024-01-26 09:27:35 +0500

    nextjs

---
+++ next-routing/src/app/about/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About = () => {
+  return <div className="text-3xl font-bold text-center p-8">About</div>;
+};
+
+export default About;
---
+++ next-routing/src/app/profile/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div className="text-4xl font-bold text-center p-6">Profile</div>;
+};
+
+export default page;
