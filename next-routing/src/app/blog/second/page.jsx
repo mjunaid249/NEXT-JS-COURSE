9810da368e44d9d1398d7d5d9c253b8a1e83a120
mjunaid249 <junaidch249249@gmail.com>
2024-01-26 10:20:16 +0500

    dynamic routing

--- next-routing/src/app/blog/second/page.jsx
+++ next-routing/src/app/blog/second/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const page = () => {
-  return <div className="text-3xl font-bold">Second Blog</div>;
+  return <div className="text-3xl p-8 text-center font-bold">Second Blog</div>;
 };
 
 export default page;
