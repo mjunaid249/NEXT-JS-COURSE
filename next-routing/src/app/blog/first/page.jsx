94ae2d5cb7e691d3b6ccfe80154c97f14d525656
mjunaid249 <junaidch249249@gmail.com>
2024-01-26 09:58:58 +0500

    routing

---
+++ next-routing/src/app/blog/first/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div className="text-3xl font-bold text-center p-8">First Blog</div>;
+};
+
+export default page;
---
+++ next-routing/src/app/blog/second/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div className="text-3xl font-bold">Second Blog</div>;
+};
+
+export default page;
